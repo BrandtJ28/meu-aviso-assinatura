7142954199d84c9d6fe1d0594b6dda030fe4d16e
BrandtJ28 <148444798+BrandtJ28@users.noreply.github.com>
2026-03-09 07:04:53 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado!');
-});
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png',
-        vibrate: [200, 100, 200]
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJETO",
+    messagingSenderId: "SEU_ID",
+    appId: "SEU_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Esta função exibe a notificação mesmo com o app totalmente fechado
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icon.png'
     };
 
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
