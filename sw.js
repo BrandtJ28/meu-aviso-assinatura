800a7b42a8236baa88929f977b767aa60e6a9291
BrandtJ28 <148444798+BrandtJ28@users.noreply.github.com>
2026-03-09 07:11:16 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,25 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
+// COLE AQUI o seu firebaseConfig que aparece na sua foto
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO",
-    messagingSenderId: "SEU_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyCvb_5kfqZ0TcvqqXYJ3H0zj...", // Complete com o seu da foto
+    authDomain: "stop-pay.firebaseapp.com",
+    projectId: "stop-pay",
+    storageBucket: "stop-pay.appspot.com",
+    messagingSenderId: "924558165559",
+    appId: "1:924558165559:web:f9cf09b7007..."
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Esta função exibe a notificação mesmo com o app totalmente fechado
+// Exibe a notificação quando o app está fechado
 messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon.png'
+        icon: 'https://cdn-icons-png.flaticon.com/512/1827/1827347.png'
     };
-
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
